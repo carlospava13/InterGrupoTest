97a29d4177aee7ea1011050322eba692982373c1
carlos pava <carpavaroty@gmail.com>
2018-04-21 15:12:53 -0500

    first project

---
+++ InterGrupo-Test/InterGrupo-TestTests/InterGrupo_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  InterGrupo_TestTests.swift
+//  InterGrupo-TestTests
+//
+//  Created by carlos pava on 21/04/18.
+//  Copyright © 2018 carlos pava. All rights reserved.
+//
+
+import XCTest
+@testable import InterGrupo_Test
+
+class InterGrupo_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
